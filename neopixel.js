c00d7934134fed7fd5b5d521a1f2ee9bc73a7b90
Joemx <jose.c.mondragon@gmail.com>
2020-05-22 10:13:45 -0500

    log 2

--- neopixel.js
+++ neopixel.js
@@ -117,7 +117,7 @@ class FCController{
           if( FCController.syncFunction != null )
             FCController.syncFunction();
           let data = new Uint8Array(totalLedCount * 3);
-          console.log("Frame " + frame);
+
           if(FCController.mode == FCController.BREATH_MODE)
             FCController.getInstance().breatheBetweenTwoColors(FCController.color1, FCController.color2, frame, frameTotals, data, totalLedCount);
           if(FCController.mode == FCController.CHASE_MODE)
@@ -199,7 +199,7 @@ class FCController{
 
 
     var actualPixel = Math.floor((frame*1.0/totalFrames))*totalLedCount;
-    console.log("{>}"+actualPixel);
+    console.log("{>}"+actualPixel + " // " + frame  + " // " + totalFrames);
     for (let pixel = 0; pixel < totalLedCount; pixel ++) {
 
 
